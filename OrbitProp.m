7cfd898890c933dd86a197fd582b4419935fc8b9
jjoannes <jeremiejoannes@gmail.com>
2017-02-25 20:17:12 +0000

    Working feature: takes timestring (in s) and orbital parameters, returns lat, lon, RMAG

--- OrbitProp.m
+++ OrbitProp.m
@@ -1,13 +1,12 @@
-%function [time_s,lat,long,rmag] = OrbitProp(timeseries,SMA,ECC,INC,RAAN,AOP,TA)
-function [r,nu] = OrbitProp(timeseries,SMA,ECC,INC,RAAN,AOP,TA)
-    %  Returns time from epoch (s), lat (deg), long (deg), earth radius
+function [lat,lon,r] = OrbitProp(timeseries,SMA,ECC,INC,RAAN,AOP,TA)
+    %  Returns lat (deg), long (deg), earth radius
     %  magnitude given time series (s) and orbital elements SMA, ECC, INC,
     %  RAAN, AOP, TA. Inputs in km and deg.
 
     % Turn into rads
     INC = deg2rad(INC); % [rad]
-    AOP = deg2rad(AOP);
-    RAAN = deg2rad(RAAN);
+    AOP0 = deg2rad(AOP);
+    RAAN0 = deg2rad(RAAN);
     TA = deg2rad(TA);
     
     % Setup initial mean anomaly
@@ -24,7 +23,7 @@ function [r,nu] = OrbitProp(timeseries,SMA,ECC,INC,RAAN,AOP,TA)
     lamb_gw_0 = deg2rad(lamb_gw_deg_0);
     
     % Orbital period
-    N = sqrt(mu/SMA^3)/(2*pi)*86400
+    N = sqrt(mu/SMA^3)/(2*pi)*86400;
 
     
     % Loop over time series
@@ -51,31 +50,28 @@ function [r,nu] = OrbitProp(timeseries,SMA,ECC,INC,RAAN,AOP,TA)
     nu(iii) = 2*atan(sqrt(1+ECC)/sqrt(1-ECC)*tan(E/2));
     r(iii) = SMA*(1-ECC^2)/(1+ECC*cos(nu(iii)));
 
-%     lamb_gw(iii) = wrapTo2Pi(lamb_gw_0 + We*T_now);
-%     
-%     RAAN(iii) = wrapTo2Pi(RAAN_0 - 3/2*J2*(R_e/a(end))^2*N*2*pi*...
-%         (1-ECC^2)^(-2)*cos(i)*(T_now-T_Ep));
-% 
-%     AOP(iii) = wrapTo2Pi(AOP_0 + 3/2*J2*(R_e/a(end))^2*N*2*pi*...
-%         (1-ECC^2)^(-2)*(2-5/2*sin(i)^2)*(T_now-T_Ep));
+    lamb_gw(iii) = wrapTo2Pi(lamb_gw_0 + We*T_now);
+    
+    RAAN(iii) = wrapTo2Pi(RAAN0 - 3/2*J2*(R_e/SMA)^2*N*2*pi*...
+        (1-ECC^2)^(-2)*cos(INC)*(T_now-T_Ep));
+
+    AOP(iii) = wrapTo2Pi(AOP0 + 3/2*J2*(R_e/SMA)^2*N*2*pi*...
+        (1-ECC^2)^(-2)*(2-5/2*sin(INC)^2)*(T_now-T_Ep));
 
     iii=iii+1;
     end
-% 
-%     %Calc Latitude
-%     Lat = asin(sin(AOP+nu)*sin(i));
-% 
-%     %Calc Longitude
-%     lambda_sin = tan(Lat)/tan(i);
-%     lambda_cos = cos(AOP+nu)./cos(Lat);
-%     Long1 = atan2(lambda_sin,lambda_cos)+RAAN-lamb_gw;
-%     Long2 = wrapToPi(Long1);
-% 
-%     nu = wrapTo2Pi(nu);
-%     nu = rad2deg(nu);
-% 
-%     Lat_deg=rad2deg(Lat);
-%     Long_deg=rad2deg(Long2);
+
+    %Calc Latitude
+    lat = asin(sin(AOP+nu)*sin(INC));
+
+    %Calc Longitude
+    lambda_sin = tan(lat)/tan(INC);
+    lambda_cos = cos(AOP+nu)./cos(lat);
+    lon = atan2(lambda_sin,lambda_cos)+RAAN-lamb_gw;
+    lon = wrapToPi(lon);
+
+    lat=rad2deg(lat);
+    lon=rad2deg(lon);
     
     function [M] = TA2M(TA)
         E = atan2(sqrt(1+ECC)*sin(TA),sqrt(1-ECC)*cos(TA));
