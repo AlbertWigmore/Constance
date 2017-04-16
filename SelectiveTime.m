98454ad3ce35470967bea0e59c9be2442957519c
jeremie joannes <jeremiejoannes@gmail.com>
2017-04-16 22:31:56 +0200

    Working time sampling function, example/test script addded

--- SelectiveTime.m
+++ SelectiveTime.m
@@ -1,54 +1,21 @@
-close all
-clear all
-clc
-
-
-sat.SMA= 26562;
-sat.ECC = 0.74;
-sat.INC = 63.4;
-sat.RAAN = 0;
-sat.AOP = 270;
-sat.TA = 90;
-
-tsteps = 0:0.001:0.8;
-
-[nu,S_lat,S_lon,rmag]=OrbitProp(tsteps,sat);
-
+function [tsteps_new,nu_new,S_lat_new,S_lon_new,rmag_new] = SelectiveTime(tsteps,nu,S_lat,S_lon,rmag,howclose)
 Brk = find(abs(diff(nu))>.9*pi);
 Brk = [1,Brk,numel(tsteps)];
 
-nu_target = 0:deg2rad(10):2*pi;
-
+nu_target = 0:deg2rad(howclose):2*pi;
 ind = 1;
 
-% for BrkNo = 1:numel(Brk)
+for BrkNo = 1:numel(Brk)-1
     for targetCount = 1:numel(nu_target)
-        [val,index(ind)] = min(abs(nu(Brk(1):Brk(2))-nu_target(targetCount)));
-        ind = ind+1;  
-    end
-    for targetCount = 1:numel(nu_target)
-        [val,index(ind)] = min(abs(nu(Brk(2):Brk(3))-nu_target(targetCount)));
-        index(ind)= index(ind)+Brk(2)-1;
+        [~,index(ind)] = min(abs(nu(Brk(BrkNo):Brk(BrkNo+1))-nu_target(targetCount)));
+        index(ind)= index(ind)+Brk(BrkNo)-1;
         ind = ind+1;
     end
-% end
+end
 
 index = unique(index);
 
 tsteps_new = tsteps(index); nu_new = nu(index); S_lat_new = S_lat(index);
-S_lon_new = S_lon(index);
+S_lon_new = S_lon(index); rmag_new = rmag(index);
 
-
-figure
-plot(tsteps,rad2deg(nu),'.-')
-hold on
-plot(tsteps_new,rad2deg(nu_new),'rs')
-
-figure 
-hold on
-box on
-load coast
-plot(long, lat, 'k', 'LineWidth',0.2) % Plot coastlines
-plot(S_lon,S_lat,'b.')
-plot(S_lon_new,S_lat_new,'rs')
-axis([-180 180 -90 90])
+end
