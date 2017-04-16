a9f5684975be93df2bb0ee8879408e7f011cbac2
jeremie joannes <jeremiejoannes@gmail.com>
2017-04-16 22:17:41 +0200

    Hard-coded working solution

--- SelectiveTime.m
+++ SelectiveTime.m
@@ -14,15 +14,26 @@ tsteps = 0:0.001:0.8;
 
 [nu,S_lat,S_lon,rmag]=OrbitProp(tsteps,sat);
 
-brk = find(abs(diff(nu))>.9*pi);
+Brk = find(abs(diff(nu))>.9*pi);
+Brk = [1,Brk,numel(tsteps)];
 
 nu_target = 0:deg2rad(10):2*pi;
+
 ind = 1;
 
-for targetCount = 1:numel(nu_target)
-    [val,index(ind)] = min(abs(nu(1:brk(1))-nu_target(targetCount)));
-    ind = ind+1;
-end
+% for BrkNo = 1:numel(Brk)
+    for targetCount = 1:numel(nu_target)
+        [val,index(ind)] = min(abs(nu(Brk(1):Brk(2))-nu_target(targetCount)));
+        ind = ind+1;  
+    end
+    for targetCount = 1:numel(nu_target)
+        [val,index(ind)] = min(abs(nu(Brk(2):Brk(3))-nu_target(targetCount)));
+        index(ind)= index(ind)+Brk(2)-1;
+        ind = ind+1;
+    end
+% end
+
+index = unique(index);
 
 tsteps_new = tsteps(index); nu_new = nu(index); S_lat_new = S_lat(index);
 S_lon_new = S_lon(index);
@@ -30,12 +41,14 @@ S_lon_new = S_lon(index);
 
 figure
 plot(tsteps,rad2deg(nu),'.-')
- 
-% figure 
-% hold on
-% box on
-% load coast
-% plot(long, lat, 'k', 'LineWidth',0.2) % Plot coastlines
-% plot(S_lon,S_lat,'b.')
-% plot(S_lon_new,S_lat_new,'rs')
-% axis([-180 180 -90 90])
+hold on
+plot(tsteps_new,rad2deg(nu_new),'rs')
+
+figure 
+hold on
+box on
+load coast
+plot(long, lat, 'k', 'LineWidth',0.2) % Plot coastlines
+plot(S_lon,S_lat,'b.')
+plot(S_lon_new,S_lat_new,'rs')
+axis([-180 180 -90 90])
