f9ffb93b96b736731c7f3ca0d7607df5fe35fd14
Albert Wigmore <albert@albertwigmore.co.uk>
2017-04-21 18:45:15 +0100

    Created Multi-Objective Function

--- MultiObjFunc.m
+++ MultiObjFunc.m
@@ -1,4 +1,4 @@
-function [phi] = ObjFunc(x)
+function [phi] = MultiObjFunc(x)
 
 sat1.Ra= x(1);
 sat1.Rp = x(2);
@@ -32,9 +32,9 @@ e_lon = linspace(-180, 180, e_lon_size + 2);
 [grid_lat, grid_lon] = meshgrid(e_lat, e_lon);
 coverage = zeros(e_lon_size + 2, e_lat_size + 2);
 earth = wgs84Ellipsoid('km'); % Earth Ellipsoid based on WGS84 Model.
-fov = 50; % FoV of sensor
+fov = 65; % FoV of sensor
 
-tsteps = [0:0.001:0.1];
+tsteps = [0:0.0005:0.1];
 for i = 1:numel(sat)
     [nu, S_lat, S_lon, rmag] = OrbitProp(tsteps, sat(i));
     [tsteps_new, ~, S_lat_new, S_lon_new, rmag_new] = SelectiveTime(tsteps, nu, S_lat, S_lon, rmag, 0.5);
@@ -50,19 +50,13 @@ R = georasterref('RasterSize', size(coverage'), ...
   'Latlim', [-90 90], 'Lonlim', [-180 180]);
 
 area = 100 * (areamat(coverage >= 1, R, earth) / 510.1E6);
-% overlap = 100 * (areamat(coverage > 1, R, earth) / 510.1E6);
+overlap = 100 * (areamat(coverage > 1, R, earth) / 510.1E6);
 
-p = 0;
-% Inequality constraints
-g = (95 - area); % Coverage greater than 95% 
-
-for j = 1:numel(g)
-    p = p + (max(0, g(j)))^2;
-end
-
-f = sum(cost) / 1E6; % Objective 2: Minimise cost
+f(1) = area; % Objective 1: Minimise overlap
+f(2) = sum(cost) / 1E6; % Objective 2: Minimise cost
+a = 1; % Weighting coefficients
 
 % Pseudo Objective Function
-phi = f + p;
+phi = a * f(1) + (1 - a) * f(2);
 
-phi = coverage; % This is required for plotting
+% phi = coverage; % This is required for plotting
